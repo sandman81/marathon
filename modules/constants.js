58c5875e2a7e5a1c2961c58e6710e914e3435bcc
evgeny81 <sandmangood@gmail.com>
2021-06-17 19:41:55 +0300

    work-8

---
+++ modules/constants.js
@@ -0,0 +1,6 @@
+export const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+export const ATTACK = ['head', 'body', 'foot'];
--- modules/utils.js
+++ modules/utils.js
@@ -1,1 +1,7 @@
-export const getRandom = (num) => Math.ceil(Math.random() * num)
+export const getRandom = (num) => Math.ceil(Math.random() * num);
+
+export const getTime = () => {
+    const date = new Date();
+
+    return `${date.getHours()}:${date.getMinutes()}`;
+}
